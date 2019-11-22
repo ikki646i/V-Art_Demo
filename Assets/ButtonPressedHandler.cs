679dc79710c61852aa706bc0937fb6a817ff8ac4
Robin Spruytte <robin.spruytte646@gmail.com>
2019-11-22 21:18:07 +0100

    It's 9PM

---
+++ Assets/ButtonPressedHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using System.Collections;
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class ButtonPressedHandler : MonoBehaviour
+    {
+        public HoverButton hoverButton;
+
+        public GameObject player;
+        public GameObject target;
+
+        public bool useVideo = false;
+        public VideoPlayer video;
+
+        private void Start()
+        {
+            hoverButton.onButtonDown.AddListener(OnButtonDown);
+        }
+
+        private void OnButtonDown(Hand hand)
+        {
+            player.transform.position = target.transform.position;
+            if (useVideo) {
+                video.frame = 2250;
+                video.Play();
+            }
+        }
+    }
+}
